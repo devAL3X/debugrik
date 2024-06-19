97f95712621497a3d4fe95bc058441207c8ed982
devAL3X <avasilega10@gmail.com>
2024-06-19 21:01:21 +0300

    print() added

--- src/debugger.hpp
+++ src/debugger.hpp
@@ -2,9 +2,9 @@
 #define DEBUGGER_H
 
 #include "cfg.hpp"
-#include "utils.hpp"
-#include "dwarfinfo.hpp"
 #include "disassm.hpp"
+#include "dwarfinfo.hpp"
+#include "utils.hpp"
 
 #include <map>
 #include <sys/ptrace.h>
@@ -14,46 +14,44 @@
 #include <sys/wait.h>
 
 #define MAX_BREAKPOINTS 100
-#define MAX_XREAD_K     8
+#define MAX_XREAD_K 8
 
 #define MSG_SHOULD_BE_RUNNED "target not started"
-#define MSG_ALREADY_STARTED  "target is already in run"
+#define MSG_ALREADY_STARTED "target is already in run"
 
+#define expand_regs(regs)                                                      \
+    {{"rdi", regs.rdi}, {"rsi", regs.rsi}, {"rdx", regs.rdx},                  \
+     {"rcx", regs.rcx}, {"rax", regs.rax}, {" r8", regs.r8},                   \
+     {" r9", regs.r9},  {"r10", regs.r10}, {"r11", regs.r11},                  \
+     {"r12", regs.r12}, {"r13", regs.r13}, {"r14", regs.r14},                  \
+     {"r15", regs.r15}, {"rbx", regs.rbx}, {"rbp", regs.rbp},                  \
+     {"rsp", regs.rsp}, {"rip", regs.rip}, {"efl", regs.eflags}};
 
-#define expand_regs(regs) { \
-    {"rdi", regs.rdi}, {"rsi", regs.rsi}, {"rdx", regs.rdx}, \
-    {"rcx", regs.rcx}, {"rax", regs.rax}, {" r8", regs.r8}, \
-    {" r9", regs.r9},  {"r10", regs.r10}, {"r11", regs.r11}, \
-    {"r12", regs.r12}, {"r13", regs.r13}, {"r14", regs.r14}, \
-    {"r15", regs.r15}, {"rbx", regs.rbx}, {"rbp", regs.rbp}, \
-    {"rsp", regs.rsp}, {"rip", regs.rip}, {"efl", regs.eflags} \
-};
+#define flatten_regs(regs, rm)                                                 \
+    regs.rdi = rm["rdi"];                                                      \
+    regs.rsi = rm["rsi"];                                                      \
+    regs.rdx = rm["rdx"];                                                      \
+    regs.rcx = rm["rcx"];                                                      \
+    regs.rax = rm["rax"];                                                      \
+    regs.r8 = rm["r8"];                                                        \
+    regs.r9 = rm["r9"];                                                        \
+    regs.r10 = rm["r10"];                                                      \
+    regs.r11 = rm["r11"];                                                      \
+    regs.r12 = rm["r12"];                                                      \
+    regs.r13 = rm["r13"];                                                      \
+    regs.r14 = rm["r14"];                                                      \
+    regs.r15 = rm["r15"];                                                      \
+    regs.rbx = rm["rbx"];                                                      \
+    regs.rbp = rm["rbp"];                                                      \
+    regs.rsp = rm["rsp"];                                                      \
+    regs.eflags = rm["efl"];                                                   \
+    regs.rip = rm["rip"]
 
-#define flatten_regs(regs, rm) \
-  regs.rdi = rm["rdi"]; \
-  regs.rsi = rm["rsi"]; \
-  regs.rdx = rm["rdx"]; \
-  regs.rcx = rm["rcx"]; \
-  regs.rax = rm["rax"]; \
-  regs.r8 = rm["r8"]; \
-  regs.r9 = rm["r9"]; \
-  regs.r10 = rm["r10"]; \
-  regs.r11 = rm["r11"]; \
-  regs.r12 = rm["r12"]; \
-  regs.r13 = rm["r13"]; \
-  regs.r14 = rm["r14"]; \
-  regs.r15 = rm["r15"]; \
-  regs.rbx = rm["rbx"]; \
-  regs.rbp = rm["rbp"]; \
-  regs.rsp = rm["rsp"]; \
-  regs.eflags = rm["efl"]; \
-  regs.rip = rm["rip"]
-
-#define run_requirement(is_running, msg) \
-    if(!is_running) { \
-        std::cout << msg << std::endl; \
-        continue; \
-    } \
+#define run_requirement(is_running, msg)                                       \
+    if (!is_running) {                                                         \
+        std::cout << msg << std::endl;                                         \
+        continue;                                                              \
+    }
 
 struct breakpoint {
     unsigned long addr;
@@ -92,8 +90,7 @@ class Debugger {
     void x_read();
     void x_set();
     void next(int *status);
+    void print();
 };
-
-
 
 #endif
