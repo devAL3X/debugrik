f60a78c81ec5ccfa3d39d8a947cc92e68e1ebbfd
devAL3X <avasilega10@gmail.com>
2024-06-20 11:12:53 +0300

    disassm tests and dump tests added

---
+++ src/test_disassm.cpp
@@ -0,0 +1,47 @@
+#include "disassm.hpp"
+#include <gtest/gtest.h>
+
+TEST(DisassmTest, ValidCodeDisassembly) {
+    Disassm disassm;
+    uint8_t code[] = {0x55, 0x48, 0x89, 0xe5, 0x48, 0x83, 0xec, 0x10,
+                      0x48, 0x89, 0x7d, 0xf8, 0x48, 0x8b, 0x45, 0xf8};
+    uint64_t code_size = sizeof(code);
+    uint64_t address = 0x1000;
+
+    testing::internal::CaptureStdout();
+    disassm.print_disassembly(code, code_size, address);
+    std::string output = testing::internal::GetCapturedStdout();
+
+    ASSERT_TRUE(output.find("push\trbp") != std::string::npos);
+    ASSERT_TRUE(output.find("mov\trbp, rsp") != std::string::npos);
+    ASSERT_TRUE(output.find("sub\trsp, 0x10") != std::string::npos);
+    ASSERT_TRUE(output.find("mov\trax, qword ptr [rbp - 8]") !=
+                std::string::npos);
+
+    ASSERT_TRUE(output.find("Error") == std::string::npos);
+}
+
+TEST(DisassmTest, InvalidCodeDisassembly) {
+    Disassm disassm;
+    uint8_t invalid_code[] = {0x55, 0x48, 0x89, 0xe5};
+    uint64_t invalid_code_size = sizeof(invalid_code);
+    uint64_t invalid_address = 0x2000;
+
+    testing::internal::CaptureStdout();
+    disassm.print_disassembly(invalid_code, invalid_code_size, invalid_address);
+    std::string output = testing::internal::GetCapturedStdout();
+
+    ASSERT_FALSE(output.find("Error") != std::string::npos);
+}
+
+TEST(DisassmTest, NextInstructionAddressIsNullptr) {
+    Disassm disassm;
+    uint8_t code[] = {0x55, 0x48, 0x89, 0xe5, 0x48, 0x83, 0xec, 0x20, 0x64,
+                      0x48, 0x8b, 0x04, 0x25, 0x28, 0x00, 0x00, 0x00, 0x48};
+    uint64_t code_size = sizeof(code);
+    uint64_t address = 0x1000;
+
+    uint64_t *next_addr = disassm.next_instr_addr(code, code_size, address);
+
+    ASSERT_EQ(next_addr, nullptr);
+}
--- src/tests.cpp
+++ src/tests.cpp
@@ -1,21 +1,22 @@
-#include <gtest/gtest.h>
+#include "utils.hpp"
+#include <cstdarg>
+#include <cstring>
 #include <gmock/gmock.h>
+#include <gtest/gtest.h>
+#include <iomanip>
+#include <iostream>
 #include <sys/ptrace.h>
 #include <sys/types.h>
 #include <unistd.h>
-#include <cstring>
-#include <iostream>
-#include <iomanip>
 #include <vector>
-#include <cstdarg>
-#include "utils.hpp"
 
 using testing::_;
 
-
 class MockPtrace {
-public:
-    MOCK_METHOD(long, ptrace, (enum __ptrace_request request, pid_t pid, void *addr, void *data));
+  public:
+    MOCK_METHOD(long, ptrace,
+                (enum __ptrace_request request, pid_t pid, void *addr,
+                 void *data));
 };
 MockPtrace *mock_ptrace = nullptr;
 
@@ -23,38 +24,36 @@ MockPtrace *mock_ptrace = nullptr;
     Хукаем, чтобы вместо настоящей функции вызывалась наша обертка
 */
 extern "C" {
-    long ptrace(enum __ptrace_request req, ...) {
-        va_list args;
-        va_start(args, req);
-        
-        pid_t pid = va_arg(args, pid_t);
-        void *addr = va_arg(args, void*);
-        void *data = va_arg(args, void*);
-        
-        va_end(args);
-        return mock_ptrace->ptrace(req, pid, addr, data);
-    }
+long ptrace(enum __ptrace_request req, ...) {
+    va_list args;
+    va_start(args, req);
+
+    pid_t pid = va_arg(args, pid_t);
+    void *addr = va_arg(args, void *);
+    void *data = va_arg(args, void *);
+
+    va_end(args);
+    return mock_ptrace->ptrace(req, pid, addr, data);
+}
 }
 
 class AllUtilsTest : public ::testing::Test {
-protected:
+  protected:
     void SetUp() {
         mock_ptrace = new MockPtrace();
         errno = 0;
     }
 
-    void TearDown() {
-        delete mock_ptrace;
-    }
+    void TearDown() { delete mock_ptrace; }
 };
 
 /*
     Здесь начинается логика тестов
 */
 
-#define TEST_SAMPLE_VALUE  0xcafebabecafebabe
-#define TEST_SAMPLE_VADDR  0x1234
-#define TEST_PID           0xdead
+#define TEST_SAMPLE_VALUE 0xcafebabecafebabe
+#define TEST_SAMPLE_VADDR 0x1234
+#define TEST_PID 0xdead
 
 TEST_F(AllUtilsTest, ReadProcessMemory_8bytesSuccess) {
     uint8_t buffer[8];
@@ -63,8 +62,9 @@ TEST_F(AllUtilsTest, ReadProcessMemory_8bytesSuccess) {
         .Times(1)
         .WillRepeatedly(testing::Return(TEST_SAMPLE_VALUE));
 
-    ASSERT_TRUE(read_process_memory(TEST_PID, TEST_SAMPLE_VADDR, buffer, sizeof(buffer)));
-    EXPECT_EQ(*((u_int64_t *) &buffer[0]), TEST_SAMPLE_VALUE);
+    ASSERT_TRUE(read_process_memory(TEST_PID, TEST_SAMPLE_VADDR, buffer,
+                                    sizeof(buffer)));
+    EXPECT_EQ(*((u_int64_t *)&buffer[0]), TEST_SAMPLE_VALUE);
 }
 
 TEST_F(AllUtilsTest, ReadProcessMemory_24bytesSuccess) {
@@ -74,10 +74,11 @@ TEST_F(AllUtilsTest, ReadProcessMemory_24bytesSuccess) {
         .Times(3) // 24 bytes splits into 4 calls by 8 bytes
         .WillRepeatedly(testing::Return(TEST_SAMPLE_VALUE));
 
-    ASSERT_TRUE(read_process_memory(TEST_PID, TEST_SAMPLE_VADDR, buffer, sizeof(buffer)));
+    ASSERT_TRUE(read_process_memory(TEST_PID, TEST_SAMPLE_VADDR, buffer,
+                                    sizeof(buffer)));
 
-    for(int i = 0; i < 3; i++)
-        EXPECT_EQ(((u_int64_t *) buffer)[i], TEST_SAMPLE_VALUE);
+    for (int i = 0; i < 3; i++)
+        EXPECT_EQ(((u_int64_t *)buffer)[i], TEST_SAMPLE_VALUE);
 }
 
 TEST_F(AllUtilsTest, ReadProcessMemory_ptrace_failure) {
@@ -87,5 +88,32 @@ TEST_F(AllUtilsTest, ReadProcessMemory_ptrace_failure) {
         .Times(1)
         .WillRepeatedly(testing::Return(-1)); // simulate error from ptrace
 
-    ASSERT_FALSE(read_process_memory(TEST_PID, TEST_SAMPLE_VADDR, buffer, sizeof(buffer)));
+    ASSERT_FALSE(read_process_memory(TEST_PID, TEST_SAMPLE_VADDR, buffer,
+                                     sizeof(buffer)));
+}
+
+TEST(DumpTest, DumpEmptyBuffer) {
+    uint64_t buffer[0];
+    testing::internal::CaptureStdout();
+    dump(nullptr, buffer, 0);
+    std::string output = testing::internal::GetCapturedStdout();
+    ASSERT_EQ(output, "\n");
+}
+
+TEST(DumpTest, DumpSingleValue) {
+    uint64_t buffer[1] = {0x1234567890ABCDEF};
+    testing::internal::CaptureStdout();
+    dump(nullptr, buffer, 1);
+    std::string output = testing::internal::GetCapturedStdout();
+    ASSERT_EQ(output, "\n0: 1234567890abcdef \n");
+}
+
+TEST(DumpTest, DumpMultipleValues) {
+    uint64_t buffer[4] = {0x1234567890ABCDEF, 0x1122334455667788,
+                          0xAABBCCDDEEFF0011, 0x9988776655443322};
+    testing::internal::CaptureStdout();
+    dump(nullptr, buffer, 4);
+    std::string output = testing::internal::GetCapturedStdout();
+    ASSERT_EQ(output, "\n0: 1234567890abcdef 1122334455667788 \n0: "
+                      "aabbccddeeff0011 9988776655443322 \n");
 }
