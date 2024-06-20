d7af1f45379fb6062cc602b05ecdbf4d3a656704
Vladislav Shevchenko <vshevchenko@astralinux.ru>
2024-06-20 11:32:27 +0300

    Final tests added

--- src/cfg_tests.cpp
+++ src/cfg_tests.cpp
@@ -3,28 +3,39 @@
 #include <gtest/gtest.h>
 #include <gmock/gmock.h>
 #include <cstring>
+#include <iostream>
 
+bool panic_triggered = false;
 
-class MockPanic {
-public:
-    MOCK_METHOD(void, panic, (char *cmd));
-};
-MockPanic *mock_panic = nullptr;
-
+void panic(char* arg) {
+    // ... mock panic
+    panic_triggered = true;
+}
 
 #define TEST_PATH_CORRECT   "/etc/hosts"
 #define TEST_PATH_INCORRECT "/k/a/c"
 
-TEST(ConfigTestSuite, get_path_correct) {
+TEST(ConfigTestSuite, cfg_load_correct_file) {
     int argc = 2;
     char *argv[argc];
     
     argv[0] = strdup("testik");
-    argv[1] = strdup("testik");
+    argv[1] = strdup(TEST_PATH_CORRECT);
 
     // Configuration cfg(TEST_PATH_CORRECT);
 
     Configuration cfg(argc, argv);
+    ASSERT_TRUE(strcmp(cfg.get_path(), TEST_PATH_CORRECT) == 0);
+}
 
-    ASSERT_TRUE(true);
+TEST(ConfigTestSuite, cfg_bad_file_specified) {
+    int argc = 2;
+    char *argv[argc];
+    
+    argv[0] = strdup("testik");
+    argv[1] = strdup(TEST_PATH_INCORRECT);
+
+    Configuration cfg(argc, argv);
+    ASSERT_TRUE(strcmp(cfg.get_path(), TEST_PATH_CORRECT) != 0);
+    ASSERT_TRUE(panic_triggered);
 }
